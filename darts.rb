6e7bc4f240b486587a5ee2d597c268c8655fb8af
Navy Nissan <cdx-dev@googlegroups.com>
2013-01-25 14:37:31 -0500

    All but the final score

--- darts.rb
+++ darts.rb
@@ -1,5 +1,7 @@
 class Darts
   PI = 3.14159
+  RADIUSES = [0.5, 1.25, 4.025, 4.2, 6.525, 6.7]
+  SLICES = [6, 13, 4, 18, 1, 20, 5, 12, 9, 14, 11, 8, 16, 7, 19, 3, 17, 2, 15, 10]
 
   def self.get_theta x, y
     raise 'unknown theta for 0,0' if x == 0 && y == 0
@@ -16,8 +18,14 @@ class Darts
     Math.sqrt(x*x + y*y)
   end
 
-  def self.slice(theta)
-    offset = (theta + 9) / 18
-    [6, 13, 4][offset]
+  def self.slice theta
+    offset = ((theta + 9) % 360) / 18
+    SLICES[offset]
+  end
+
+  def self.ring distance
+    RADIUSES.each_with_index do |radius, index|
+      return index + 1 if radius >= distance
+    end
   end
 end
--- darts_spec.rb
+++ darts_spec.rb
@@ -52,12 +52,66 @@ describe Darts do
   end
 
   describe '.slice' do
+    it 'should return 6 for a theta of 1' do
+      Darts.slice(1).should == 6
+    end
+
     it 'should return 13 for a theta of 11' do
       Darts.slice(11).should == 13
     end
 
     it 'should return 4 for a theta of 28' do
       Darts.slice(28).should == 4
+    end
+
+    it 'should return 20 for a theta of 90' do
+      Darts.slice(90).should == 20
+    end
+
+    it 'should return 11 for a theta of 180' do
+      Darts.slice(180).should == 11
+    end
+
+    it 'should return 3 for a theta of 270' do
+      Darts.slice(270).should == 3
+    end
+
+    it 'should return 6 for a theta of 359' do
+      Darts.slice(359).should == 6
+    end
+
+    it 'should return 6 for a theta of 360' do
+      Darts.slice(360).should == 6
+    end
+
+    it 'should return 6 for a theta of 720' do
+      Darts.slice(720).should == 6
+    end
+  end
+
+  describe '.ring' do
+    it 'should return ring 1 for 0.4' do
+      Darts.ring(0.4).should == 1
+    end
+
+    it 'should return ring 2 for 1.25' do
+      Darts.ring(1.25).should == 2
+    end
+
+    it 'should return ring 3 for 4.025' do
+      Darts.ring(4.025).should == 3
+    end
+
+    it 'should return ring 4 for 4.2' do
+      Darts.ring(4.2).should == 4
+    end
+
+    it 'should return ring 5 for 6.525' do
+      Darts.ring(6.525).should == 5
+    end
+
+    it 'should return ring 6 for 6.7' do
+      Darts.ring(6.7).should == 6
     end
   end
 end
