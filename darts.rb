1ee1e0e7bfb01a0c4cbed5c0390db2f33469b9c9
Navy Nissan <cdx-dev@googlegroups.com>
2013-01-25 14:35:02 -0500

    Josh's Refactoring

--- darts.rb
+++ darts.rb
@@ -1,7 +1,15 @@
 class Darts
   PI = 3.14159
+  RADIUSES = [0.5, 1.25, 4.025, 4.2, 6.525, 6.7]
+  SLICES = [6, 13, 4, 18, 1, 20, 5, 12, 9, 14, 11, 8, 16, 7, 19, 3, 17, 2, 15, 10]
+  attr_accessor :x, :y
 
-  def self.get_theta x, y
+  def initialize x, y
+    @x = x
+    @y = y
+  end
+
+  def get_theta
     raise 'unknown theta for 0,0' if x == 0 && y == 0
     if x == 0
       y > 0 ? 90 : 270
@@ -12,12 +20,18 @@ class Darts
     end
   end
 
-  def self.distance_from_bullseye x, y
+  def distance_from_bullseye
     Math.sqrt(x*x + y*y)
   end
 
-  def self.slice(theta)
-    offset = (theta + 9) / 18
-    [6, 13, 4][offset]
+  def slice
+    offset = ((theta + 9) % 360) / 18
+    SLICES[offset]
+  end
+
+  def ring
+    RADIUSES.each_with_index do |radius, index|
+      return index + 1 if radius >= distance
+    end
   end
 end
--- darts_spec.rb
+++ darts_spec.rb
@@ -3,61 +3,115 @@ require './darts.rb'
 describe Darts do
   describe '.get_theta' do
     it 'should return the correct theta for 1, 3' do
-      Darts.get_theta(1,3).should be_within(0.01).of 71.57
+      Darts.new(1,3).get_theta.should be_within(0.01).of 71.57
     end
 
     it 'should return the correct theta for 1, 4' do
-      Darts.get_theta(1,4).should be_within(0.01).of 75.96
+      Darts.new(1,4).get_theta.should be_within(0.01).of 75.96
     end
 
     it 'should return the correct theta for 0, 4' do
-      Darts.get_theta(0,4).should == 90
+      Darts.new(0,4).get_theta.should == 90
     end
 
     it 'should return the correct theta for 0, -4' do
-      Darts.get_theta(0,-4).should == 270
+      Darts.new(0,-4).get_theta.should == 270
     end
 
     it 'should return the correct theta for 4, 0' do
-      Darts.get_theta(4,0).should == 0
+      Darts.new(4,0).get_theta.should == 0
     end
 
     it 'should return the correct theta for -4, 0' do
-      Darts.get_theta(-4,0).should == 180
+      Darts.new(-4,0).get_theta.should == 180
     end
 
     it 'should return the correct theta for 0, 0' do
       lambda {
-        Darts.get_theta(0,0)
+        Darts.new(0,0).get_theta
       }.should raise_error
     end
   end
 
   describe '.distance_from_bullseye' do
     it 'should give the correct distance to the bullseye for 1, 3' do
-      Darts.distance_from_bullseye(1,3).should be_within(0.01).of 3.16
+      Darts.new(1,3).distance_from_bullseye.should be_within(0.01).of 3.16
     end
 
     it 'should give the correct distance to the bullseye for 3, 4' do
-      Darts.distance_from_bullseye(3,4).should be_within(0).of 5
+      Darts.new(3,4).distance_from_bullseye.should be_within(0).of 5
     end
 
     it 'should give the correct distance to the bullseye for 0, 0' do
-      Darts.distance_from_bullseye(0,0).should be_within(0).of 0
+      Darts.new(0,0).distance_from_bullseye.should be_within(0).of 0
     end
 
     it 'should give the correct distance to the bullseye for -3, -4' do
-      Darts.distance_from_bullseye(-3,-4).should be_within(0).of 5
+      Darts.new(-3,-4).distance_from_bullseye.should be_within(0).of 5
     end
   end
 
   describe '.slice' do
+    it 'should return 6 for a theta of 1' do
+      Darts.slice(1).should == 6
+    end
+
     it 'should return 13 for a theta of 11' do
       Darts.slice(11).should == 13
     end
 
     it 'should return 4 for a theta of 28' do
       Darts.slice(28).should == 4
+    end
+
+    it 'should return 20 for a theta of 90' do
+      Darts.slice(90).should == 20
+    end
+
+    it 'should return 11 for a theta of 180' do
+      Darts.slice(180).should == 11
+    end
+
+    it 'should return 3 for a theta of 270' do
+      Darts.slice(270).should == 3
+    end
+
+    it 'should return 6 for a theta of 359' do
+      Darts.slice(359).should == 6
+    end
+
+    it 'should return 6 for a theta of 360' do
+      Darts.slice(360).should == 6
+    end
+
+    it 'should return 6 for a theta of 720' do
+      Darts.slice(720).should == 6
+    end
+  end
+
+  describe '.ring' do
+    it 'should return ring 1 for 0.4' do
+      Darts.ring(0.4).should == 1
+    end
+
+    it 'should return ring 2 for 1.25' do
+      Darts.ring(1.25).should == 2
+    end
+
+    it 'should return ring 3 for 4.025' do
+      Darts.ring(4.025).should == 3
+    end
+
+    it 'should return ring 4 for 4.2' do
+      Darts.ring(4.2).should == 4
+    end
+
+    it 'should return ring 5 for 6.525' do
+      Darts.ring(6.525).should == 5
+    end
+
+    it 'should return ring 6 for 6.7' do
+      Darts.ring(6.7).should == 6
     end
   end
 end
