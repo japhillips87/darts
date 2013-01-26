86ff462e44d560d89599ec8a6cf8309a8c10b386
Josh Phillips <dev+japhillips87@dealermatch.com>
2013-01-25 22:28:17 -0500

    finished the kata

--- darts.rb
+++ darts.rb
@@ -2,9 +2,33 @@ class Darts
   PI = 3.14159
   RADIUSES = [0.5, 1.25, 4.025, 4.2, 6.525, 6.7]
   SLICES = [6, 13, 4, 18, 1, 20, 5, 12, 9, 14, 11, 8, 16, 7, 19, 3, 17, 2, 15, 10]
+  attr_accessor :x, :y, :theta, :distance
 
-  def self.get_theta x, y
-    raise 'unknown theta for 0,0' if x == 0 && y == 0
+  def initialize x, y
+    @x = x
+    @y = y
+    @theta = get_theta
+    @distance = distance_from_bullseye
+  end
+
+  def score
+    return 50 if theta == 'bullseye'
+    case ring
+    when 1
+      50
+    when 2
+      25
+    when 4
+      slice * 3
+    when 6
+      slice * 2
+    else
+      slice
+    end
+  end
+
+  def get_theta
+    return 'bullseye' if x == 0 && y == 0
     if x == 0
       y > 0 ? 90 : 270
     elsif y == 0
@@ -14,16 +38,16 @@ class Darts
     end
   end
 
-  def self.distance_from_bullseye x, y
+  def distance_from_bullseye
     Math.sqrt(x*x + y*y)
   end
 
-  def self.slice theta
+  def slice
     offset = ((theta + 9) % 360) / 18
     SLICES[offset]
   end
 
-  def self.ring distance
+  def ring
     RADIUSES.each_with_index do |radius, index|
       return index + 1 if radius >= distance
     end
