ddaccc66867369cbec650b2f95e5e85bfdb24264
Josh Phillips <dev+japhillips87@dealermatch.com>
2013-01-26 17:44:52 -0500

    translated constant PI to Math::PI

--- darts.rb
+++ darts.rb
@@ -1,5 +1,4 @@
 class Darts
-  PI = 3.14159
   RADIUSES = [0.5, 1.25, 4.025, 4.2, 6.525, 6.7]
   SLICES = [6, 13, 4, 18, 1, 20, 5, 12, 9, 14, 11, 8, 16, 7, 19, 3, 17, 2, 15, 10]
   attr_accessor :x, :y, :theta, :distance
@@ -34,7 +33,7 @@ class Darts
     elsif y == 0
       x > 0 ? 0 : 180
     else
-      Math.atan(y/x) * (180/PI)
+      Math.atan(y/x) * (180/Math::PI)
     end
   end
 
